a422ab74eb506c436170ff3d6367995a3020673c
StoyanDimitrov0016 <stoyan1020@gmail.com>
2026-03-14 18:31:59 +0200

    feat: expand seeded exercise catalog

--- convex/exercises.ts
+++ convex/exercises.ts
@@ -5,7 +5,7 @@ import { query } from "./_generated/server";
 export const list = query({
   args: {},
   handler: async (ctx) => {
-    return await ctx.db.query("exercises").collect();
+    return await ctx.db.query("exercises").withIndex("by_name").collect();
   },
 });
 
